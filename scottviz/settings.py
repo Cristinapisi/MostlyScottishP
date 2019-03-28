718117f782d4ba23bfa50f54604f47bae3751ece
cristina <cristina.mihailescu@gmail.com>
2019-03-28 14:37:29 +0200

    More library updates

--- scottviz/settings.py
+++ scottviz/settings.py
@@ -96,7 +96,7 @@ STATICFILES_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(BASE_DIR, "static"),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -116,13 +116,13 @@ TEMPLATE_LOADERS = (
     #     'django.template.loaders.eggs.Loader',
 )
 MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'hitcounter.middleware.HitCountMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
--- scottviz/wsgi.py
+++ scottviz/wsgi.py
@@ -12,8 +12,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scottviz.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
